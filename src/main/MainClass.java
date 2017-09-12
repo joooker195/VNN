66ccadcd31de1e2d1e98d6ffa9ca15ebe0f3484b
joooker195 <joooker195@gmail.com>
2017-09-13 01:28:22 +0400

    added view

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,7 +1,31 @@
 package main;
 
+import controll.Controller;
+import logs.Log;
+import view.View;
+
 /**
  * Created by Ксю on 12.09.2017.
  */
-public class MainClass {
+public class MainClass
+{
+    /*public static String training = null;
+    public static String testing = null;
+     public static Log log = new Log();*/
+    public static void runTraining(String file) throws Exception
+    {
+        Controller.getData(file);
+        Controller.trainingNetwork();
+    }
+    public static void runTesting(String file) throws Exception
+    {
+        Controller.getData(file);
+        Controller.testingNetwork();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Log.isDebugEnable = true;
+        View.setvisibleView();
+    }
 }
---
+++ src/view/View.java
@@ -0,0 +1,55 @@
+package view;
+
+import logs.Log;
+import main.MainClass;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ксю on 12.09.2017.
+ */
+public class View
+{
+   // private static Scanner in  = new Scanner(System.in);
+    public static void setvisibleView() throws Exception {
+
+        System.out.println("Welcome!");
+        System.out.println("1-training");
+        System.out.println("2-tesiting");
+        if(inputCommand().equals("1"))
+        {
+            System.out.println("1-brent");
+            System.out.println("2-wti");
+            if(inputCommand().equals("1")) {
+                MainClass.runTraining("dataBrent.xls");
+            }
+            else if(inputCommand().equals("2"))
+            {
+                MainClass.runTraining("dataWTI.xls");
+            }
+        }
+        else if(inputCommand().equals("2"))
+        {
+            System.out.println("1-brent");
+            System.out.println("2-wti");
+            if(inputCommand().equals("1")) {
+                MainClass.runTesting("dataBrent.xls");
+            }
+            else if(inputCommand().equals("2"))
+            {
+                MainClass.runTesting("dataWTI.xls");
+            }
+        }
+
+    }
+    public static String inputCommand()
+    {
+        Scanner in  = new Scanner(System.in);
+        return in.next();
+    }
+
+    public static void printSomeInfo(String info)
+    {
+        System.out.println(info);
+    }
+}
